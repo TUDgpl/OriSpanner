a5f64a6b406dd33f044ce6d8a0cf0dbfce305f8e
Guangping Li <guangping.li@tu-dortmund.de>
2024-03-11 10:10:38 +0100

    bash file: add result folders

--- exp.sh
+++ exp.sh
@@ -8,10 +8,10 @@
 cd /work/mguali01/Ori_spanner/OriSpanner
 for s in 8 16 32 64
 do 
-	srun --output /work/mguali01/solutions/instance$s_%a.dp.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a dp 
-	srun --output /work/mguali01/solutions/instance$s_%a.sat.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat
-	srun --output /work/mguali01/solutions/instance$s_%a.sat_2.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 2
-	srun --output /work/mguali01/solutions/instance$s_%a.sat_3.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 3
-	srun --output /work/mguali01/solutions/instance$s_%a.sat_4.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 4
-	srun --output /work/mguali01/solutions/instance$s_%a.sat_5.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 5
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.dp.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a dp -d /work/mguali01/solutions/uniform_$s/
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.sat.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -d /work/mguali01/solutions/uniform_$s/
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.sat_2.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 2 -d /work/mguali01/solutions/uniform_$s/
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.sat_3.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 3 -d /work/mguali01/solutions/uniform_$s/
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.sat_4.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 4 -d /work/mguali01/solutions/uniform_$s/
+	srun --output /work/mguali01/solutions/uniform_$s/instance$s_%a.sat_5.txt ./OriSpanner -f instances/uniform_$s/uniform_1_$s_$SLURM_ARRAY_TASK_ID.txt -a sat -s -l 5 -d /work/mguali01/solutions/uniform_$s/
 done
