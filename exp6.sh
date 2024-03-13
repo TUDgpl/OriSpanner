095868cee07f96a3c6ec4435eb0215cf9fc482aa
Guangping Li <guangping.li@tu-dortmund.de>
2024-03-13 13:58:44 +0100

    add extra short edge exp

---
+++ exp6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --array=0-99
+#SBATCH --partition=short
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=6000
+#SBATCH --job-name=OriSp64
+#SBATCH --output=/work/mguali01/Ori_spanner/OriSpanner/output
+cd /work/mguali01/Ori_spanner/OriSpanner
+for s in 8 16 32 64 128 256 512
+do 
+	srun --output /work/mguali01/Ori_spanner/OriSpanner/solutions/uniform_$s/instance${s}_%a.sat_6.txt /work/mguali01/Ori_spanner/OriSpanner/build_linux/Release/src/OriSpanner -f instances/uniform_$s/uniform_1_${s}_${SLURM_ARRAY_TASK_ID}.txt -a sat -s -l 6 -d solutions/uniform_$s/
+	srun --output /work/mguali01/Ori_spanner/OriSpanner/solutions/uniform_$s/instance${s}_%a.sat_7.txt /work/mguali01/Ori_spanner/OriSpanner/build_linux/Release/src/OriSpanner -f instances/uniform_$s/uniform_1_${s}_${SLURM_ARRAY_TASK_ID}.txt -a sat -s -l 7 -d solutions/uniform_$s/
+done
