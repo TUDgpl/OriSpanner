ad50b2668d227fe565d8bbc98906a8db8eb26986
Guangping Li <guangping.li@tu-dortmund.de>
2024-03-13 23:11:54 +0100

    short_16

--- exp6.sh
+++ exp6.sh
@@ -9,7 +9,7 @@
 cd /work/mguali01/Ori_spanner/OriSpanner
 for s in 8 16 32 64 128 256 512
 do
-	for l in 8 9 10 11 12 13 14 15 16
+	for l in 8 9 10 11 12 13 14 15 16 17 18 19 20
 	do 
 		srun --output /work/mguali01/Ori_spanner/OriSpanner/solutions/uniform_$s/instance${s}_%a.sat_${l}.txt /work/mguali01/Ori_spanner/OriSpanner/build_linux/Release/src/OriSpanner -f instances/uniform_$s/uniform_1_${s}_${SLURM_ARRAY_TASK_ID}.txt -a sat -s -l ${l} -d solutions/uniform_$s/
 	done
